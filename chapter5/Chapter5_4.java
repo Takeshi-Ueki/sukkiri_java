4a7ee1eb68660e586260b035bd027ad1462565fc
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-10-03 15:47:20 +0900

    10/3 work

---
+++ chapter5/Chapter5_4.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+public class Chapter5_4 {
+  public static void main(String[] args) {
+    hello("湊");
+    hello("朝香");
+    hello("菅原");
+  }
+
+  public static void hello(String name) {
+    System.out.println(name + "さん、こんにちは");
+  }
+}
